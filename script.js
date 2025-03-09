0ebb468b8d072a8485bf5efab06ad1a1fd5a2b89
sunotsue <suminpar@cs.cmu.edu>
2025-03-09 16:45:10 -0700

    skeleton done

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementById('navbar');
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+}); 
